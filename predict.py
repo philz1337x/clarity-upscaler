7eee2b9effba062b313f3021518288520566b8dc
p <pberben@gmx.de>
2024-03-18 23:19:32 +0700

    sd model

--- predict.py
+++ predict.py
@@ -64,6 +64,11 @@ class Predictor(BasePredictor):
             choices=[16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 256],
             default=144
         ),
+        sd_model: str = Input(
+            description="Stable Diffusion model checkpoint",
+            choices=['epicrealism_naturalSinRC1VAE.safetensors [84d76a0328]', 'juggernaut_reborn.safetensors [338b85bc4f]', 'juggernaut_final.safetensors'],
+            default="juggernaut_reborn.safetensors [338b85bc4f]",
+        ),
         scheduler: str = Input(
             description="scheduler",
             choices=['DPM++ 2M Karras', 'DPM++ SDE Karras', 'DPM++ 2M SDE Exponential', 'DPM++ 2M SDE Karras', 'Euler a', 'Euler', 'LMS', 'Heun', 'DPM2', 'DPM2 a', 'DPM++ 2S a', 'DPM++ 2M', 'DPM++ SDE', 'DPM++ 2M SDE', 'DPM++ 2M SDE Heun', 'DPM++ 2M SDE Heun Karras', 'DPM++ 2M SDE Heun Exponential', 'DPM++ 3M SDE', 'DPM++ 3M SDE Karras', 'DPM++ 3M SDE Exponential', 'DPM fast', 'DPM adaptive', 'LMS Karras', 'DPM2 Karras', 'DPM2 a Karras', 'DPM++ 2S a Karras', 'Restart', 'DDIM', 'PLMS', 'UniPC'],
@@ -85,7 +90,7 @@ class Predictor(BasePredictor):
 
         payload = {
             "override_settings": {
-                "sd_model_checkpoint": "juggernaut_reborn.safetensors [338b85bc4f]",
+                "sd_model_checkpoint": sd_model,
                 "sd_vae": "vae-ft-mse-840000-ema-pruned.safetensors",
                  "CLIP_stop_at_last_layers": 1,
             },
