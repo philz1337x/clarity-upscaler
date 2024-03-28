15b134b2019b54c50887f6a5f7022f78761725c5
p <pberben@gmx.de>
2024-03-28 14:09:35 +0700

    x

--- download_weights.py
+++ download_weights.py
@@ -14,7 +14,14 @@ def download_file(url, folder_path, filename):
     else:
         print(f"Error downloading the file. Status code: {response.status_code}")
 
-url = "https://huggingface.co/philz1337x/upscaler/resolve/main/RealESRGAN_x4plus_anime_6B.pth?download=true"
-folder_path = "models/ESRGAN"
-filename = "RealESRGAN_x4plus_anime_6B.pth"
-download_file(url, folder_path, filename)
+# Download the first file
+url_1 = "https://huggingface.co/philz1337x/upscaler/resolve/main/RealESRGAN_x4plus_anime_6B.pth?download=true"
+folder_path_1 = "models/ESRGAN"
+filename_1 = "RealESRGAN_x4plus_anime_6B.pth"
+download_file(url_1, folder_path_1, filename_1)
+
+# Download the second file
+url_2 = "https://huggingface.co/philz1337x/embeddings/resolve/main/verybadimagenegative_v1.3.pt?download=true"
+folder_path_2 = "embeddings"
+filename_2 = "verybadimagenegative_v1.3.pt"
+download_file(url_2, folder_path_2, filename_2)
