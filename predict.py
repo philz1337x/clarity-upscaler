03cc25b0634cc2c8809f92e32e9aa33753b2827c
p <pberben@gmx.de>
2024-04-13 19:03:16 +0700

    Multi-step improvement

--- predict.py
+++ predict.py
@@ -290,11 +290,8 @@ class Predictor(BasePredictor):
             
             if not first_iteration:
                 creativity = creativity * 0.8
-                tiling_width = tiling_width + 16
-                tiling_width = max(256, tiling_width)
-                tiling_height = tiling_height + 16
-                tiling_height = max(256, tiling_height)
-                first_iteration = False
+                
+            first_iteration = False
 
             payload = {
                 "override_settings": {
